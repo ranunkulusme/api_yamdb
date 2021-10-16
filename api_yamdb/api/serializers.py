1e6cc9addd6cde27cea62fe8f2c945ddd860b668
ZloyStorm <saltanovd@bk.ru>
2021-10-16 14:55:57 +0300

    finaly!

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 import datetime
 
 from rest_framework import serializers
+
 from reviews.models import Category, Comments, Genre, Review, Title, User
 
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,16 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.db.models import Avg
 from rest_framework_simplejwt.tokens import AccessToken
-from .filters import TitleFilter
 
 from reviews.models import Category, Genre, Review, Title, User
 
+from .filters import TitleFilter
 from .permissions import (AdminOrModeratorOrRead, IsAdminOrSuperuser,
                           IsAdminUserOrReadOnly, OwnerOrReadOnly)
 from .serializers import (CategorySerializer, CommentSerializer,
